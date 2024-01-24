88a68fe48b278e2fa45afb47dfc5e1c40fac25ff
HoussemDAAS <houssemdaas2@gmail.com>
2024-01-24 13:18:27 +0100

    adding condition

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,20 @@ const [initialInvestment, setinitialInvestment] = useState(10000);
 const [annualInvestment, setannualInvestment] = useState(1200);
 const [expectedReturn, setexpectedReturn] = useState(6);
 const [duration, setduration] = useState(10);
-
-
+const valide=initialInvestment>=1 && annualInvestment>=1 && expectedReturn>=1 && duration>=1;
+console.log(valide);
 const Data = calculateInvestmentResults({  initialInvestment: parseFloat(initialInvestment) || 0,
   annualInvestment: parseFloat(annualInvestment) || 0,
   expectedReturn: parseFloat(expectedReturn) || 0,
   duration: parseInt(duration) || 0,});
- 
+  // const valide=false;
+  // const validate = (initialInvestment, annualInvestment, expectedReturn, duration) => {
+  //  if (!initialInvestment || !annualInvestment || !expectedReturn || !duration) {
+  //    return false;
+  //  }else {
+  //    return true;
+  //  }
+  // }
   return (
     <div>
  <div id="input-group" className="center">
@@ -39,7 +46,9 @@ const Data = calculateInvestmentResults({  initialInvestment: parseFloat(initial
           <input type="number" name="user" id="duration" value={duration} onChange={(e) => setduration(e.target.value)}/>
         </div>
       </div>
-    <Result Result={Data} />
+
+    {valide ? <Result Result={Data} /> : <p className="center">please enter valid data</p>}
+    {/*  */}
     </div>
   )
 }
